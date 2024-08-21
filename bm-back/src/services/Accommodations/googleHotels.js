56d277be15cd73831d016bfc5b6d9d0da6e6a5dd
Dmitry Ashavskiy <160394751+ashavskydb@users.noreply.github.com>
2024-08-21 11:24:15 +0300

    Update googleHotels.js

--- bm-back/src/services/Accommodations/googleHotels.js
+++ bm-back/src/services/Accommodations/googleHotels.js
@@ -3,7 +3,7 @@ import { Accommodation } from '../../models/Accommodation.js';
 
 const API_KEY = 'c2ff5dcfd254658cd22d35f43e06fa89a42a7396ce96588aaab08c60f01cc82f'; 
 
-const search_Accommodations = async (city, checkInDate, checkOutDate) => {
+async function searchAccommodations (city, checkInDate, checkOutDate) {
   try {
     const response = await axios.get('https://serpapi.com/search', {
       params: {
@@ -22,7 +22,7 @@ const search_Accommodations = async (city, checkInDate, checkOutDate) => {
     console.log('Search Accommodations Response:', response.data);
     return response.data.best_hotels || response.data.other_hotels;
   } catch (error) {
-    console.error('Error in search_Accommodations:', error); 
+    console.error('Error in searchAccommodations:', error); 
     throw error;
   }
 }
@@ -33,7 +33,7 @@ async function fetchAccommodationData(city, checkInDate, checkOutDate) {
       throw new Error('All parameters (city, checkInDate, checkOutDate) are required');
     }
 
-    const accommodations = await search_Accommodations(city, checkInDate, checkOutDate);
+    const accommodations = await searchAccommodations(city, checkInDate, checkOutDate);
     if (!accommodations || accommodations.length === 0) {
       throw new Error('No accommodations found');
     }
@@ -73,4 +73,4 @@ async function addAccommodationToCalendar(accommodation) {
   console.log(`Event added to calendar: Stay at ${accommodation.city} from ${accommodation.startDate.toISOString()} to ${accommodation.endDate.toISOString()}`);
 }
 
-export { search_Accommodations, bookAccommodationAndCreateTrip, fetchAccommodationData };
+export { searchAccommodations, bookAccommodationAndCreateTrip, fetchAccommodationData };
